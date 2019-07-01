61b0250977a8913258a6f9ff6210659d7f499380
LaNice Powell <lanicemp@aol.com>
2019-07-01 17:39:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,16 @@
-def square_array(numbers)
-  numbers = [1,2,3]
+def square_array(array)
+  counter = 0
+  
+  while array[counter] do 
+    array[counter]** 2
+counter += num1
+end 
+end
+
+def square_array(array)
+  arr[]
+numbers = [1,2,3]
+  
 square_array(numbers)
 end 
 #def square_array(array)
