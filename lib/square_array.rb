8af1dc996e392b0258631675e6cd3a7754b1b0bc
LaNice Powell <lanicemp@aol.com>
2019-07-01 17:32:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
-  # your code here
-end
+def square_array(numbers)
+  numbers = [1,2,3]
+square_array(numbers)
+end 
+#def square_array(array)
+  #array = [ 1, 2, 3]
+  #square_array=(array * array)
+#end 
+#num1=4
+#num2=30
+#multiplication = (num1 * num2)
+#end
+  
+ # print squared_numbers
+#end
